fc2f7ce29c0065a81fa7d24d8dc7625b1e506d1a
John Diederich <47949092+shootntime@users.noreply.github.com>
2020-03-09 21:29:50 -0500

    added php

---
+++ Content.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Contact</title>
+</head>
+
+<body>
+    <h1>contact</h1>
+    <a href="about.html"> about</a>
+    <a href="index.html"> home</a>
+    <?php
+        echo "This is text in php";
+    ?>
+</body>
+
+</html>
